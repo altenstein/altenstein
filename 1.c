388d9d1d9ec8ebf177fc198db4f35c70c9dd5265
Oduen <filtermr@gmail.com>
2023-01-13 23:26:20 +0300

    Build 0.0.0.1

--- 1.c
+++ 1.c
@@ -1,8 +1,44 @@
 #include<stdio.h>
+#include<stdlib.h>
 
 int main(void)
 {
-	printf("Hello World!");
+	char current_map[21][100] = 
+	{
+     "###############################################",
+     "#@                                          WW#",
+     "#            WWW                            WW#",
+     "#            WWWW           a              WW #",
+     "#           WWW                   #        WW #",
+     "#                                # #      WW  #",
+     "#                     T         #   #    WW   #",
+     "#   T            f              #  B#   WW    #",
+     "#                               ## ##  WW     #",
+     "#                                       WW    #",
+     "#                                  T    WW    #",
+     "#                   T            T       WW   #",
+     "#     T                       T       T  WW   #",
+     "#               T              T T T    WW    #",
+     "#                                  T  TWW T   #",
+     "#  p                             T     TWW    #",
+     "#                              T    T T WWT   #",
+     "#      T                     T     T WWWWWW   #",
+     "# +                           T   WWWWWWWWWWWW#",
+     "#                                WWWWWWWWWWWWW#",
+     "###############################################"
+	};
+	
+	for(int i = 0; i < 21; i++)
+	{
+		for(int j = 0; j < 100; j++)
+		{
+			printf("%c", current_map[i][j]);
+		}
+		printf("\n");
+		system("Color 0C");
+	}
+	printf("\n\n\n\n\n\n\n\n");
+	
 	getchar();
 	return 0;
 }
